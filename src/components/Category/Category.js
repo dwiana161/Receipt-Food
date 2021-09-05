7ec092c061aa169f2f1ba76916a4f4e724d56a39
dwiana <dwiana.kamila.tif18@polban.ac.id>
2021-09-05 20:28:08 +0700

    Implementation caraousel in category section

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Card from './Card';
 import './Category.css';
+import Cupcakes from '../image/cupcakes.jpg';
+import Pizza from '../image/pizza.jpg';
+import Kebab from '../image/kebab.png';
+import Salmon from '../image/salmon.jpg';
+import Doughnut from '../image/dougnut.jpg';
 
 const Category = () => {
     return (
@@ -8,41 +13,55 @@ const Category = () => {
             <h2 className="title">Browser Our Category</h2>
             <h4 className="receipt">Receipt</h4>
             <div className="row justify-content-start">
-                <div className="col-sm-4 col-md-4">
-                    <Card 
-                        title='Cupcake'
-                        item='22 Items'
-                    />
+                <div className="caraousel slide w-100">
+                    <div className="carousel-inner w-100" role="listbox">
+                        <div className="caraousel-item">
+                            <div className="col-md-4">
+                                <Card 
+                                    title='Cupcakes'
+                                    item='22 Items'
+                                />
+                            </div>
+                        </div>
+                        <div className="caraousel-item">
+                            <div className="col-md-4">
+                                <Card 
+                                    title='Pizza'
+                                    item='25 Items'
+                                />
+                            </div>
+                        </div>
+                        <div className="caraousel-item">
+                            <div className="col-md-4">
+                                <Card 
+                                title='Kebab'
+                                item='12 Items' 
+                                />
+                            </div>
+                        </div>
+                        <div className="caraousel-item">
+                            <div className="col-md-4">
+                                <Card 
+                                    title='Salmon'
+                                    item='22 Items'
+                                />
+                            </div>
+                        </div>
+                        <div className="caraousel-item">
+                            <div className="col-md-4">
+                                <Card 
+                                    title='Doughnut'
+                                    item='11 Items'
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-sm-4 col-md-4">
-                    <Card 
-                        title='Pizza'
-                        item='25 Items'
-                    />
-                </div>
-                <div className="col-sm-4 col-md-4">
-                    <Card 
-                       title='Kebab'
-                       item='12 Items' 
-                    />
-                </div>
-                <div className="col-sm-4 col-md-4">
-                    <Card 
-                        title='Salmon'
-                        item='22 Items'
-                    />
-                </div>
-                <div className="col-sm-4 col-md-4">
-                    <Card 
-                        title='Doughnut'
-                        item='11 Items'
-                    />
-                </div>
-                <div className="d-flex flex-row-reverse">
-                    <button type="submit" className="btn-category btn mb-4">
+                <div className="d-flex flex-row-reverse d-none d-lg-block">
+                    <button type="submit" className="btn-category btn mb-4 float-end">
                         PREV
                     </button>
-                    <button type="submit" className="btn-category btn mb-4">
+                    <button type="submit" className="btn-category btn mb-4 float-end">
                         NEXT
                     </button>
                 </div>
